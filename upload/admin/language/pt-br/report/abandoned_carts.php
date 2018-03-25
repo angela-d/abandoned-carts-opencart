2c05f4df6af5c1ad106c00f088831de0ab6b23fe
KEVAO28 <37647437+KEVAO28@users.noreply.github.com>
2018-03-25 15:59:45 -0300

    Corrections and additions
    <?php
    // Heading
    $_['heading_title']        = 'Carrinhos abandonados/pedidos abandonados';
    $_['button_recover']       = 'Enviar mensagem para os carrinhos abandonados/pedidos abandonados';
    
    // Text
    $_['text_list']            = 'Lista de carrinhos abandonados/pedidos abandonados';
    $_['text_missing']         = 'Pedido abandonado';
    $_['text_success']         = 'Pronto! Foi enviada uma mensagem para os pedidos de carrinhos abandonados selecionados!';
    
    // Column
    $_['column_order_id']      = 'Número do pedido';
    $_['column_customer']      = 'Cliente';
    $_['column_status']        = 'Status';
    $_['column_date_added']    = 'Data do pedido';
    $_['column_date_modified'] = 'Data de modificação';
    $_['column_abandoned'] 	   = 'Mensagem enviada?';
    $_['column_total']         = 'Total';
    $_['column_action']        = 'Ação';
    
    // Error
    $_['warning_duplicate']    = 'Existem %d pedidos no sistema com o ip de %s\. Certifique-se de não enviar mensagens duplicadas, verifique a fila pendente de novos pedidos.';
    $_['error_permission']     = 'Você não tem permissão para modificar os carrinhos abandonados.';

--- upload/admin/language/pt-br/report/abandoned_carts.php
+++ upload/admin/language/pt-br/report/abandoned_carts.php
@@ -1,23 +1,23 @@
 <?php
 // Heading
-$_['heading_title']        = 'Carrinhos abandonados';
-$_['button_recover']       = 'Enviar mensagem para os carrinhos abandonados';
+$_['heading_title']        = 'Carrinhos abandonados/pedidos abandonados';
+$_['button_recover']       = 'Enviar mensagem para os carrinhos abandonados/pedidos abandonados';
 
 // Text
-$_['text_list']            = 'Lista de carrinhos abandonados';
+$_['text_list']            = 'Lista de carrinhos abandonados/pedidos abandonados';
 $_['text_missing']         = 'Pedido abandonado';
-$_['text_success']         = 'Foi enviado uma mensagem para os carrinhos selecionados!';
+$_['text_success']         = 'Pronto! Foi enviada uma mensagem para os pedidos de carrinhos abandonados selecionados!';
 
 // Column
-$_['column_order_id']      = 'Peido N°';
+$_['column_order_id']      = 'Número do pedido';
 $_['column_customer']      = 'Cliente';
 $_['column_status']        = 'Status';
 $_['column_date_added']    = 'Data do pedido';
-$_['column_date_modified'] = 'Date de modificação';
+$_['column_date_modified'] = 'Data de modificação';
 $_['column_abandoned'] 	   = 'Mensagem enviada?';
 $_['column_total']         = 'Total';
 $_['column_action']        = 'Ação';
 
 // Error
-$_['warning_duplicate']    = 'Existem %d pedidos no sistema com o ip de %s\'. Certifique-se não enviar mensagens duplicadas, verifique a fila pendente de novos pedidos.';
+$_['warning_duplicate']    = 'Existem %d pedidos no sistema com o ip de %s\. Certifique-se de não enviar mensagens duplicadas, verifique a fila pendente de novos pedidos.';
 $_['error_permission']     = 'Você não tem permissão para modificar os carrinhos abandonados.';
