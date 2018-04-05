66dd3967e6888c52ddb648fc0ff1430cdbbc6579
angela <33108490+angela-d@users.noreply.github.com>
2018-04-04 19:06:05 -0500

    restore pt-br lang
    Signed-off-by: angela <33108490+angela-d@users.noreply.github.com>

---
+++ upload/admin/language/pt-br/extension/module/abandoned_carts.php
@@ -0,0 +1,30 @@
+<?php
+// Heading
+$_['heading_title']          = 'Carrinhos Abandonados';
+
+// Text
+$_['text_bailed_carts']      = 'Carrinhos Abandonados';
+$_['text_edit']              = 'Editar opção de Carrinho Abandonado';
+$_['text_extension']         = 'Extenções';
+$_['text_list']              = 'Lista de carrinhos abandonados';
+$_['text_no_results']        = 'Boas notícias! Não há carrinhos abandonados por enquanto.';
+$_['text_success']           = 'Abandoned Carts configuration has been saved!';
+
+// Email Body
+$_['subject_prefix']         = "Ordem incompleta com";
+$_['failed_cart_greeting']   = 'Olá, %s';
+$_['failed_cart_intro']      = 'Percebemos que você estava efetuando um pedido em nossa loja recentemente e não concluiu a sua compra; nós só queríamos ter certeza de que você não teve problemas ao finalizar a seu pedido';
+$_['failed_cart_contents']   = 'Aqui está a lista que ficou no seu carrinho de compras:';
+$_['failed_cart_body']       = 'Se o seu pedido foi intencional, nós adoraríamos ouvir o seu feedback, sugestões ou reclamações que você possa oferecer para que possamos melhorar nossa loja e a experiência de nossos cliente!';
+$_['failed_cart_footer']     = 'Você recebeu esta mensagem porque você (ou alguém usando seu endereço de e-mail) efetuou pedido mas não finalizou. Você não foi adicionado a nenhum boletim informativo e não receberá mais nossas comunicações como resultado de sua tentativa de compra (a menos que você tenha se inscrito explicitamente para nossa newsletter e/ou notificações de produtos).';
+$_['failed_cart_signoff']    = 'De qualquer forma agradecemos pelo interesse em nossos produtos';
+$_['failed_cart_signature']  = 'Atenciosamente,';
+
+// Entry
+$_['entry_abandoned_status'] = 'Critérios';
+$_['help_abandoned_status']  = 'Estado da ordem a considerar abandonado';
+$_['entry_limit']            = 'Dia(s) para procurar carrinhos abandonados ';
+$_['entry_status']           = 'Status';
+
+// Button
+$_['button_view']            = 'Visualizar detalhes do pedido abandonado';
