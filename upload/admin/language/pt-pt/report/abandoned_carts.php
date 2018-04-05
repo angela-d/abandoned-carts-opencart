64bd0f1a02371dbead8f4e7b4d4b1948c69958d9
angela <33108490+angela-d@users.noreply.github.com>
2018-04-04 19:06:52 -0500

    restore pt-pt lang
    Signed-off-by: angela <33108490+angela-d@users.noreply.github.com>

---
+++ upload/admin/language/pt-pt/report/abandoned_carts.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']        = 'Carrinhos abandonados/pedidos abandonados';
+$_['button_recover']       = 'Enviar mensagem para os carrinhos abandonados/pedidos abandonados';
+
+// Text
+$_['text_list']            = 'Lista de carrinhos abandonados/pedidos abandonados';
+$_['text_missing']         = 'Pedido abandonado';
+$_['text_search']          = 'If no transactions appear in the customer search, the records counted are bailed carts';
+$_['text_success']         = 'Pronto! Foi enviada uma mensagem para os pedidos de carrinhos abandonados selecionados!';
+$_['text_deleted']         = 'Selected transaction(s) have been deleted';
+
+// Column
+$_['column_order_id']      = 'Número do pedido';
+$_['column_customer']      = 'Cliente';
+$_['column_status']        = 'Status';
+$_['column_date_added']    = 'Data do pedido';
+$_['column_date_modified'] = 'Data de modificação';
+$_['column_abandoned'] 	   = 'Mensagem enviada?';
+$_['column_total']         = 'Total';
+$_['column_action']        = 'Ação';
+
+// Error
+$_['warning_duplicate']    = 'Existem %d pedidos no sistema com o ip de %s. Certifique-se de não enviar mensagens duplicadas, verifique a fila pendente de novos pedidos.';
+$_['error_permission']     = 'Você não tem permissão para modificar os carrinhos abandonados.';
